d56eea8fd1267f306516e599f8a46967628889aa
Enrico De Nicola <edenicola16@icloud.com>
2023-06-15 21:38:49 -0300

    fdfdfff

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-import AboutMe from '../Components/AboutMe.jsx';
 import MyProjects from '../Components/MyProjects.jsx';
-import './index.css'
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/myprojects",
-    element: <MyProjects />,
-  },
-
-
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-
-    <RouterProvider router={router} />
-
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/myprojects" element={<MyProjects />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
